10591487fc0063f16ff422672285a87e4a6d7411
long-long-float <niinikazuki@yahoo.co.jp>
2023-06-27 02:20:03 +0900

    Refactor and add arithmetic tests

--- tests/compiler_test.rs
+++ tests/compiler_test.rs
@@ -3,6 +3,7 @@ mod compiler_test {
     use colored::Colorize;
     use core::time;
     use serde_json::Value;
+    use std::collections::HashSet;
     use std::fs::{create_dir, File};
     use std::io::Write;
     use std::path::Path;
@@ -11,94 +12,169 @@ mod compiler_test {
     use std::{env, thread};
 
     use function_name::named;
+    use lisp_rs::lispi;
     use lisp_rs::lispi::cli_option::CliOption;
-    use lisp_rs::lispi::compile;
+    use lisp_rs::lispi::opt;
 
     /// Timeout for execution the emulator in seconds
     const TIMEOUT_EMU: u32 = 5;
 
-    fn compile_and_run(name: &'static str, program: &str) -> Value {
-        if !name.is_empty() {
-            let dir_name = Path::new("compiler_test_files");
-            if !dir_name.exists() {
-                create_dir(dir_name).expect("Cannot create test dir");
+    struct Compiler {
+        name: &'static str,
+        program: &'static str,
+        optimizations: HashSet<opt::Optimize>,
+    }
+
+    impl Compiler {
+        fn min_opts(self) -> Self {
+            Self {
+                optimizations: opt::Optimize::minimum(),
+                ..self
             }
-            let mut file = File::create(dir_name.join(format!("{}.scm", name))).unwrap();
-            file.write_all(program.as_bytes()).unwrap();
         }
 
-        let program = program.split('\n').map(|l| l.to_string()).collect();
+        fn run(self) -> Value {
+            let Self {
+                name,
+                program,
+                optimizations,
+            } = self;
 
-        let dump = env::var("DUMP").is_ok();
-
-        let opt = CliOption {
-            filename: None,
-            compile: true,
-            dump,
-            without_opts: false,
-        };
-
-        assert!(compile(program, &opt).is_ok());
-        assert!(Path::new("out.bin").exists());
-        assert!(Path::new("out.elf").exists());
-
-        let mut child = Command::new("./rv32emu/build/rv32emu")
-            .args(["--dump-registers", "-", "--quiet", "out.elf"])
-            .stdout(Stdio::piped())
-            .spawn()
-            .expect("Failed to spawn rv32emu.");
-
-        let mut over_timeout = true;
-        for _ in 0..(TIMEOUT_EMU * 10) {
-            match child.try_wait() {
-                Ok(Some(status)) => {
-                    assert!(status.success());
-
-                    over_timeout = false;
-                    break;
+            if !name.is_empty() {
+                let dir_name = Path::new("compiler_test_files");
+                if !dir_name.exists() {
+                    create_dir(dir_name).expect("Cannot create test dir");
                 }
-                Ok(None) => {}
-                Err(e) => panic!("error attempting to wait: {e}"),
+                let mut file = File::create(dir_name.join(format!("{}.scm", name))).unwrap();
+                file.write_all(program.as_bytes()).unwrap();
             }
 
-            thread::sleep(time::Duration::from_millis(100));
+            let program = program.split('\n').map(|l| l.to_string()).collect();
+
+            let dump = env::var("DUMP").is_ok();
+
+            let opt = CliOption {
+                filename: None,
+                compile: true,
+                dump,
+                without_opts: false,
+            };
+
+            assert!(lispi::compile(program, &opt, optimizations).is_ok());
+            assert!(Path::new("out.bin").exists());
+            assert!(Path::new("out.elf").exists());
+
+            let mut child = Command::new("./rv32emu/build/rv32emu")
+                .args(["--dump-registers", "-", "--quiet", "out.elf"])
+                .stdout(Stdio::piped())
+                .spawn()
+                .expect("Failed to spawn rv32emu.");
+
+            let mut over_timeout = true;
+            for _ in 0..(TIMEOUT_EMU * 10) {
+                match child.try_wait() {
+                    Ok(Some(status)) => {
+                        assert!(status.success());
+
+                        over_timeout = false;
+                        break;
+                    }
+                    Ok(None) => {}
+                    Err(e) => panic!("error attempting to wait: {e}"),
+                }
+
+                thread::sleep(time::Duration::from_millis(100));
+            }
+
+            if over_timeout {
+                child.kill().expect("Failed to kill rv32emu.");
+                panic!("{}", "Timeout during execution on rv32emu.".yellow());
+            }
+
+            let output = child.wait_with_output().unwrap();
+
+            if dump {
+                println!("{}", str::from_utf8(&output.stdout).unwrap_or(""));
+            }
+
+            serde_json::from_slice(&output.stdout).unwrap()
         }
+    }
 
-        if over_timeout {
-            child.kill().expect("Failed to kill rv32emu.");
-            panic!("{}", "Timeout during execution on rv32emu.".yellow());
+    fn compile(name: &'static str, program: &'static str) -> Compiler {
+        Compiler {
+            name,
+            program,
+            optimizations: opt::Optimize::all(),
         }
+    }
 
-        let output = child.wait_with_output().unwrap();
+    trait ValueExtension {
+        fn as_i32(&self) -> Option<i32>;
+    }
 
-        if dump {
-            println!("{}", str::from_utf8(&output.stdout).unwrap_or(""));
+    impl ValueExtension for Value {
+        fn as_i32(&self) -> Option<i32> {
+            self.as_i64().map(|v| v as i32)
         }
-
-        let registers: Value = serde_json::from_slice(&output.stdout).unwrap();
-        registers
     }
 
     #[test]
     #[named]
     fn just_return_42() {
-        let registers = compile_and_run(function_name!(), "42");
+        let registers = compile(function_name!(), "42").run();
         assert_eq!(Some(42), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
-    fn add_1_plus_2() {
-        let registers = compile_and_run(function_name!(), "(+ 1 2)");
-        assert_eq!(Some(3), registers["x10"].as_i64());
+    fn arith_add() {
+        let registers = compile(function_name!(), "(+ 1 2)").min_opts().run();
+        assert_eq!(Some(3), registers["x10"].as_i32());
+
+        let registers = compile(function_name!(), "(+ 1 -2)").min_opts().run();
+        assert_eq!(Some(-1), registers["x10"].as_i32());
+    }
+
+    #[test]
+    #[named]
+    fn arith_sub() {
+        let registers = compile(function_name!(), "(- 1 2)").min_opts().run();
+        assert_eq!(Some(-1), registers["x10"].as_i32());
+
+        let registers = compile(function_name!(), "(- 1 -2)").min_opts().run();
+        assert_eq!(Some(3), registers["x10"].as_i32());
+    }
+
+    #[test]
+    #[named]
+    fn arith_mul() {
+        let registers = compile(function_name!(), "(* 2 3)").min_opts().run();
+        assert_eq!(Some(6), registers["x10"].as_i32());
+
+        let registers = compile(function_name!(), "(* 2 -3)").min_opts().run();
+        assert_eq!(Some(-6), registers["x10"].as_i32());
+    }
+
+    #[test]
+    #[named]
+    fn arith_div() {
+        let registers = compile(function_name!(), "(/ 6 3)").min_opts().run();
+        assert_eq!(Some(2), registers["x10"].as_i32());
+
+        let registers = compile(function_name!(), "(/ 5 3)").min_opts().run();
+        assert_eq!(Some(1), registers["x10"].as_i32());
+
+        let registers = compile(function_name!(), "(/ 6 -3)").min_opts().run();
+        assert_eq!(Some(-2), registers["x10"].as_i32());
     }
 
     #[test]
     fn shift() {
-        let registers = compile_and_run("", "(<< 1 3)");
+        let registers = compile("", "(<< 1 3)").min_opts().run();
         assert_eq!(Some(8), registers["x10"].as_i64());
 
-        let registers = compile_and_run("", "(>> 8 3)");
+        let registers = compile("", "(>> 8 3)").min_opts().run();
         assert_eq!(Some(1), registers["x10"].as_i64());
 
         // TODO: Test about logical/arithmetic shift
@@ -107,21 +183,22 @@ mod compiler_test {
     #[test]
     #[named]
     fn variables() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define x 10)
 (set! x 20)
 x
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(20), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn function_many_variables() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define f (lambda (x)
@@ -134,48 +211,51 @@ x
     ))
 (f 2)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(128), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn function_simple_call() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define double (lambda (x) (+ x x)))
 (double 21)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(42), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn function_call_rank2() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define f (lambda (x) (+ x x)))
 (define g (lambda (x) (+ (f x) (f x))))
 (g 4)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(16), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn function_call_directly() {
-        let registers = compile_and_run(function_name!(), "((lambda (x) (+ x x)) 5)");
+        let registers = compile(function_name!(), "((lambda (x) (+ x x)) 5)").run();
         assert_eq!(Some(10), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn let_recursive_function() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (let sum ([x 10]) 
@@ -183,43 +263,47 @@ x
       0
     (+ x (sum (- x 1)))))
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(55), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn let_let_add() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (let* ([x 2] [y 3]) (+ x y))
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(5), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn load_large_positive_integer() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 2000000000
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(2000000000), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn load_large_negative_integer() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 -2000000000
 "#,
-        );
+        )
+        .run();
         assert_eq!(
             Some(-2000000000),
             registers["x10"].as_i64().map(|i| i as i32)
@@ -229,7 +313,7 @@ x
     #[test]
     #[named]
     fn tail_recursion_sum_by_loop() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define sum 0)
@@ -239,27 +323,29 @@ x
         (loop (+ i 1))))
 sum
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(55), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn array_len() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define ary (array 1 2 3))
 (array->len ary)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(3), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn array_in_function() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define f (lambda () 
@@ -268,20 +354,22 @@ sum
 (define g (lambda () (f)))
 (g)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(2), registers["x10"].as_i64());
     }
 
     #[test]
     #[named]
     fn array_get_1() {
-        let registers = compile_and_run(
+        let registers = compile(
             function_name!(),
             r#"
 (define ary (array 1 2 3))
 (array->get ary 1)
 "#,
-        );
+        )
+        .run();
         assert_eq!(Some(2), registers["x10"].as_i64());
     }
 }
