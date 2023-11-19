d242d6ecaa130b3fe8b2e8a9b3fe2fb1b8f32b74
long-long-float <niinikazuki@yahoo.co.jp>
2023-11-19 17:19:18 +0900

    Implement removing_ref_and_deref

--- src/lispi/pass/removing_ref_and_deref.rs
+++ src/lispi/pass/removing_ref_and_deref.rs
@@ -17,14 +17,71 @@
 //!
 
 use anyhow::Result;
+use itertools::Itertools;
+use rustc_hash::FxHashMap;
 
 use crate::lispi::ir::{
     basic_block::{Function, IrProgram},
+    instruction::{AnnotatedInstr, Instruction, Operand},
     IrContext,
 };
 
 pub fn remove_ref_and_deref(fun: &Function, ir_ctx: &mut IrContext) -> Result<()> {
-    for inst in fun.walk_instructions(&ir_ctx.bb_arena) {}
+    let mut ref_mapping = FxHashMap::default();
+
+    for inst in fun.walk_instructions(&ir_ctx.bb_arena) {
+        if let Instruction::Reference(Operand::Variable(op)) = &inst.inst {
+            ref_mapping.insert(inst.result.clone(), op.clone());
+        }
+    }
+
+    for bb in &fun.basic_blocks {
+        let bb = ir_ctx.bb_arena.get_mut(*bb).unwrap();
+
+        bb.insts = bb
+            .insts
+            .drain(..)
+            .filter_map(
+                |AnnotatedInstr {
+                     result: result_var,
+                     inst,
+                     ty,
+                     tags,
+                 }| {
+                    match inst {
+                        Instruction::Dereference(Operand::Variable(var)) => {
+                            ref_mapping.get(&var).map(|referenced_var| AnnotatedInstr {
+                                result: result_var,
+                                inst: Instruction::Operand(Operand::Variable(
+                                    referenced_var.clone(),
+                                )),
+                                ty,
+                                tags,
+                            })
+                        }
+                        Instruction::Reference(Operand::Variable(var)) => {
+                            if ref_mapping.contains_key(&result_var) {
+                                None
+                            } else {
+                                Some(AnnotatedInstr {
+                                    result: result_var,
+                                    inst: Instruction::Reference(Operand::Variable(var)),
+                                    ty,
+                                    tags,
+                                })
+                            }
+                        }
+                        _ => Some(AnnotatedInstr {
+                            result: result_var,
+                            inst,
+                            ty,
+                            tags,
+                        }),
+                    }
+                },
+            )
+            .collect_vec();
+    }
 
     Ok(())
 }
